517752be70a8a03bb88e7ebc9ff8c10d7445e013
JOJO <593400610@qq.com>
2019-04-22 23:59:11 +0800

    添加了itemParam商品规格参数功能

---
+++ cap-manager/cap-manager-web/src/main/java/com/cap/controller/ItemParamController.java
@@ -0,0 +1,40 @@
+package com.cap.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cap.common.pojo.TaotaoResult;
+import com.cap.pojo.CapItemParam;
+import com.cap.service.ItemParamService;
+
+/**
+ * 商品规格参数模板管理Controller
+ */
+@Controller
+@RequestMapping("/item/param")
+public class ItemParamController {
+
+	@Autowired
+	private ItemParamService itemParamService;
+	
+	@RequestMapping("/query/itemcatid/{itemCatId}")
+	@ResponseBody
+	public TaotaoResult getItemParamByCid(@PathVariable Long itemCatId) {
+		TaotaoResult result = itemParamService.getItemParamByCid(itemCatId);
+		return result;
+	}
+	
+	@RequestMapping("/save/{cid}")
+	@ResponseBody
+	public TaotaoResult insertItemParam(@PathVariable Long cid, String paramData) {
+		//创建pojo对象
+		CapItemParam itemParam = new CapItemParam();
+		itemParam.setItemCatId(cid);
+		itemParam.setParamData(paramData);
+		TaotaoResult result = itemParamService.insertItemParam(itemParam);
+		return result;
+	}
+}
