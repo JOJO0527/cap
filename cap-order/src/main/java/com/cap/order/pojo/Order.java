ef4b9178b009320ccdbacd7fcce635b721c4a23b
JOJO <593400610@qq.com>
2019-05-01 00:22:44 +0800

    增加了订单系统

---
+++ cap-order/src/main/java/com/cap/order/pojo/Order.java
@@ -0,0 +1,27 @@
+package com.cap.order.pojo;
+
+import java.util.List;
+
+import com.cap.pojo.CapOrder;
+import com.cap.pojo.CapOrderItem;
+import com.cap.pojo.CapOrderShipping;
+
+public class Order extends CapOrder {
+
+	private List<CapOrderItem> orderItems;
+	private CapOrderShipping orderShipping;
+	public List<CapOrderItem> getOrderItems() {
+		return orderItems;
+	}
+	public void setOrderItems(List<CapOrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+	public CapOrderShipping getOrderShipping() {
+		return orderShipping;
+	}
+	public void setOrderShipping(CapOrderShipping orderShipping) {
+		this.orderShipping = orderShipping;
+	}
+	
+	
+}
---
+++ cap-order/src/main/java/com/cap/order/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.cap.order.service;
+
+import java.util.List;
+
+import com.cap.common.pojo.TaotaoResult;
+import com.cap.pojo.CapOrder;
+import com.cap.pojo.CapOrderItem;
+import com.cap.pojo.CapOrderShipping;
+
+public interface OrderService {
+
+	TaotaoResult createOrder(CapOrder order, List<CapOrderItem> itemList, CapOrderShipping orderShipping);
+}
